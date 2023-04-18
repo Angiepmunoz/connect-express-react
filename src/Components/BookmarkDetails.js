f63822f2e0cd7ba4c40552ad5dbc79817238bc5b
Angie Munoz <angiepmunoz@gmail.com>
2023-04-18 09:37:27 -0400

    app

---
+++ src/Components/BookmarkDetails.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+function BookmarkDetails() {
+  const [bookmark, setBookmark] = useState([]);
+  let { index } = useParams(); //getting index from URL
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/bookmarks/${index}`)
+      .then((response) => {
+        console.log(response);
+        setBookmark(response.data);
+      })
+      .catch(() => {
+        navigate("/not-found");
+      });
+  }, [index, navigate]);
+  const handleDelete = () => {
+    axios
+      .delete(`${process.env.REACT_APP_API_URL}/bookmarks/${index}`)
+      .then(() => {
+        navigate("/bookmarks");
+      });
+  };
+  return (
+    <article>
+      <h3>
+        {bookmark.isFavorite ? <span>⭐️</span> : null} {bookmark.name}
+      </h3>
+      <h5>
+        <span>
+          <a href={bookmark.url}>{bookmark.name}</a>
+        </span>{" "}
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        {bookmark.url}
+      </h5>
+      <h6>{bookmark.category}</h6>
+      <p>{bookmark.description}</p>
+      <div className="showNavigation">
+        <div>
+          {" "}
+          <Link to={`/bookmarks`}>
+            <button>Back</button>
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <Link to={`/bookmarks/${index}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default BookmarkDetails;
---
+++ src/Components/Bookmarks.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Bookmark from "./Bookmark";
+
+function Bookmarks() {
+  const [bookmarks, setBookmarks] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/bookmarks`).then((res) => {
+      setBookmarks(res.data);
+    });
+    // fetch(`${process.env.REACT_APP_API_URL}/bookmarks`)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     setBookmarks(data);
+    //   });
+  }, []);
+
+  return (
+    <div className="Bookmarks">
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Take me there</th>
+              <th>See this bookmark</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookmarks.map((bookmark, index) => {
+              return <Bookmark key={index} bookmark={bookmark} index={index} />;
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export default Bookmarks;
